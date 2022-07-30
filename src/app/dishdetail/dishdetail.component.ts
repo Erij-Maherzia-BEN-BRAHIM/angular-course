477f7a7073514dc4552c1fdada1b3feb930d3cdf
Erij Maherzia BEN BRAHIM <erijmaherziabenbrahim@gmail.com>
2022-07-30 15:23:58 +0100

    SPA part3

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Params , ActivatedRoute} from '@angular/router';
 import { Dish } from '../shared/dish';
 import { Location } from '@angular/common';
