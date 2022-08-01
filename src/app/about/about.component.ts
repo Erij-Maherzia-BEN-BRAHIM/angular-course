ce5f0f62743f117b6830ab51eff1e968a3203bd3
Erij Maherzia BEN BRAHIM <erijmaherziabenbrahim@gmail.com>
2022-08-01 18:13:45 +0100

    Animations p2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { LeaderService } from '../services/leader.service';
 import { Leader } from '../shared/leader';
+import { expand } from './../animations/app.animation';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  animations:[
+    expand()
+  ]
 })
 export class AboutComponent implements OnInit {
 
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,24 +6,22 @@ import { switchMap } from 'rxjs/operators';
 import { DishService } from '../services/dish.service';
 import { Comment } from '../shared/comment';
 import { Dish } from '../shared/dish';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { expand, flyInOut, visibility } from '../animations/app.animation';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-    ])
-  ]
+    visibility(),
+    flyInOut(),
+    expand()
+  ],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
+    
 })
 export class DishdetailComponent implements OnInit {
   @ViewChild('fform') commentFormDirective;
